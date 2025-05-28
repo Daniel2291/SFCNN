4a2c33d4b9582576e994d825007cad221226f53c
Daniel-meeg <daniel.stefanov@denoize.com>
2025-05-28 14:34:18 +0200

    Re-added e2cnn_experiments as normal directory

---
+++ e2cnn_experiments/experiments/e2cnn/__about__.py
@@ -0,0 +1,18 @@
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__url__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+]
+
+__title__ = "e2cnn-py36"
+__summary__ = "E(2)-Equivariant CNNs Library for PyTorch"
+__url__ = 'https://github.com/QUVA-Lab/e2cnn/tree/legacy_py3.6'
+__version__ = "0.1.8"
+__author__ = "Gabriele Cesa, Maurice Weiler"
+__email__ = "cesa.gabriele@gmail.com"
+__license__ = "BSD 3-Clause Clear"
